60a7e89c2258e5fa08f53a33dcd83c6076f698d0
nanjizal <none>
2020-06-10 05:32:20 +0100

    tweaks

--- src/kitGL/gluon/HelpGL.hx
+++ src/kitGL/gluon/HelpGL.hx
@@ -8,7 +8,7 @@ import gluon.webgl.GLBuffer;
 import gluon.webgl.GLShader;
 
 inline
-function clearAll( gl: GLProgram, width: Int, height: Int ){
+function clearAll( gl: GLContext, width: Int, height: Int ){
     gl.viewport( 0, 0, width, height );
     gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
     gl.clear( gl.COLOR_BUFFER_BIT );
@@ -18,15 +18,15 @@ function programSetup( gl:          GLContext
                      , strVertex:   String
                      , strFragment: String ): GLProgram {
     var program: GLProgram = gl.createProgram();
-    gl.attachShader( program, shaderSetup( gl, gl.VERTEX_SHADER, strVertex ) );
-    gl.attachShader( program, shaderSetup( gl, gl.FRAGMENT_SHADER, strFragment ) );
+    gl.attachShader( program, shaderSetup( gl, ShaderType.VERTEX_SHADER, strVertex ) );
+    gl.attachShader( program, shaderSetup( gl, ShaderType.FRAGMENT_SHADER, strFragment ) );
     gl.linkProgram( program );
-    if( !gl.getProgramParameter(program, gl.LINK_STATUS ) ) {
+    if( !gl.getProgramParameter(program, ProgramParameter.LINK_STATUS ) ) {
         throw("Error linking program. " + gl.getProgramInfoLog( program ) );
         return null;
     }
     gl.validateProgram( program );
-    if( !gl.getProgramParameter( program, gl.VALIDATE_STATUS ) ) {
+    if( !gl.getProgramParameter( program, ProgramParameter.VALIDATE_STATUS ) ) {
         throw("Error validating program. " + gl.getProgramInfoLog( program ) );
         return null;
     }
@@ -40,7 +40,7 @@ function shaderSetup( gl: GLContext
     var shader = gl.createShader( shaderType );
     gl.shaderSource( shader, str );
     gl.compileShader( shader );
-    if( !gl.getShaderParameter( shader, gl.COMPILE_STATUS ) ){
+    if( !gl.getShaderParameter( shader, ShaderParameter.COMPILE_STATUS ) ){
         throw("Error compiling shader. " + gl.getShaderInfoLog( shader ) );
         return null;
     }
